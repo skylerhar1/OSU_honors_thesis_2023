4113bb56a6dc7effb9ebbb0f789041aa34b36ccd
skylerhar1 <67615347+skylerhar1@users.noreply.github.com>
2023-05-06 22:08:33 -0700

    Add files via upload

---
+++ Assembly pipeline/get_stats.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+
+
+
+/local/cluster/bbmap/stats.sh in=/nfs1/BPP/Spatafora_Lab/hars/HI-C_assembly/PGA_Assembly_db/PGA_assembly.fasta
+
+/local/cluster/bbmap/stats.sh /nfs1/BPP/Spatafora_Lab/hars/raw_data/ _gapcloser.1.2.fa >/nfs1/BPP/Spatafora_Lab/hars/final_reference_assembly/stats_dir/redundans_stats/redundans_stats.txt
