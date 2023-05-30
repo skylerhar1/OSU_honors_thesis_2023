b3ff237e1f4ff74c02d7c89defbcf2e3b30ed6b2
skylerhar1 <67615347+skylerhar1@users.noreply.github.com>
2023-05-30 14:53:27 -0700

    Update fun_fix.sh

--- Annotation pipeline/fun_fix.sh
+++ Annotation pipeline/fun_fix.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 eval "$(conda shell.bash hook)"
-conda activate /local/cluster/funannotate-1.8.9/
+conda activate /local/cluster/funannotate-1.8.14/
 
-funannotate fix -i fun_HI/update_results/Rhizopogon_salebrosus.gbk -t fun_HI/update_results/Rhizopogon_salebrosus.tbl
+funannotate fix -i gap_closed_fun/update_results/Rhizopogon_salebrosus.gbk -t gap_closed_fun/update_results/Rhizopogon_salebrosus.tbl
 
 
