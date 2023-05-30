4381b8ef9646b038214c3c67c1e5fe4c8a88f27d
skylerhar1 <67615347+skylerhar1@users.noreply.github.com>
2023-05-30 15:03:18 -0700

    Create fun_compare.sh

---
+++ Compare statistics/fun_compare.sh
@@ -0,0 +1,3 @@
+source /local/cluster/funannotate-1.8.14/activate.sh
+
+funannotate compare -i /nfs1/BPP/Spatafora_Lab/hars/final_reference_assembly/gap_closed_fun/annotate_results/R_sal_locus_tag.gbk  --cpus 20
